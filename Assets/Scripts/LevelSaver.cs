6bc93f8735fc7891bf706f4bc65abb37c9b04ac0
Eugene Mahnovsky <Eugene Mahnovsky>
2016-03-22 00:13:15 +0200

    level editor

---
+++ Assets/Scripts/LevelSaver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class LevelSaver
+{
+	public void SaveLevel( Level level )
+	{
+		JSONObject root = new JSONObject(JSONObject.Type.OBJECT);
+
+		root.AddField("type", level.LevelMode.ToString());
+
+		level.Save(root);
+		string filePath = Application.dataPath + "/Resources/level_" + level.Number + ".json";
+		
+		File.WriteAllBytes(filePath, System.Text.Encoding.UTF8.GetBytes (root.Print(true)));
+	}
+}
